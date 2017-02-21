5d8360a278ec32d43c300e208de5b5224e1f7061
Alok Singh <a1singh@ucsd.edu>
2017-02-20 22:35:23 -0800

    Random Forest - Finalized

--- workbench/allfunctions.py
+++ workbench/allfunctions.py
@@ -38,218 +38,7 @@ def rmse_scorer(model, X, y):
 # <<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>
 # <<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>
 
-# In[50]:
-
-#1
-def LassoCVModel(filename):
-    #open file and get the dictionary
-    import pickle
-    from sklearn.linear_model import Lasso
-    from sklearn.metrics import mean_squared_error
-    from sklearn import preprocessing
-    from sklearn.model_selection import GridSearchCV
-
-    with open(filename, 'rb') as handle:
-        data = pickle.load(handle)
-
-    #extract X_train, y_train, X_test, t_test
-    X_train = data['X_train']
-    y_train = data['y_train']
-    X_test = data['X_test']
-    y_test = data['y_test']
-    print("Dataset size read: train %d and test %d \n" %(len(y_train), len(y_test)))
-    
-    #Normalize
-    X_train = preprocessing.normalize(X_train, norm='l1')
-    X_test  = preprocessing.normalize(X_test,  norm='l1')
-    
-    ##############################################################
-    tuned_parameters = []
-    tuned_parameters.append( {'alpha' : np.logspace(-4, 10, 30),
-                              'precompute' : [True, False]
-                             } )
-    
-    ##############################################################
-    
-    print("# Tuning hyper-parameters ")
-    print()
-
-    # Lasso
-    grdsurch = GridSearchCV(Lasso(alpha=1.0, fit_intercept=True, normalize=False, precompute=False, 
-                                  copy_X=True, max_iter=1e7, tol=1e-6, warm_start=False, 
-                                  positive=False, random_state=None, selection='random'), 
-                       tuned_parameters, 
-                       cv=3, 
-                       n_jobs=-1, 
-                       scoring=rmse_scorer)
-    print('Starting grdsurch.fit(X_train, y_train)')
-    
-    grdsurch.fit(X_train, y_train)
-
-    print("\nBest parameters set found on development set:")
-    print()
-    print(grdsurch.best_params_)
-    
-    print(grdsurch.best_estimator_)
-    print()
-    rmse_cv = grdsurch.best_score_
-
-    #Reporting Score on Test Set
-    model               = grdsurch.best_estimator_
-    reporting_testscore = rmse_scorer(model, X_test, y_test)
-    
-    return {filename: {'train_rmse_cv_4_picking': rmse_cv, 
-                       'test_rmse_4_reporting': reporting_testscore, 
-                       'test_mean_y_4_comparing': y_test.mean(),
-                       'model': model
-                      }
-           }
-
-
-# In[52]:
-
-#2
-def OMPCVModel(filename):
-    #open file and get the dictionary
-    import pickle
-    from sklearn.linear_model import OrthogonalMatchingPursuit
-    from sklearn.metrics import mean_squared_error
-    from sklearn import preprocessing
-    from sklearn.model_selection import GridSearchCV
-
-    with open(filename, 'rb') as handle:
-        data = pickle.load(handle)
-
-    #extract X_train, y_train, X_test, t_test
-    X_train = data['X_train']
-    y_train = data['y_train']
-    X_test = data['X_test']
-    y_test = data['y_test']
-    print("Dataset size read: train %d and test %d \n" %(len(y_train), len(y_test)))
-    
-    #Normalize
-    #X_train = preprocessing.normalize(X_train, norm='l1')
-    #X_test  = preprocessing.normalize(X_test,  norm='l1')
-    
-    #‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’ 
-    ##############################################################
-    tuned_parameters = []
-    tuned_parameters.append({'tol' : [1e-20, 1e-15, 1e-11],
-                             'n_nonzero_coefs' : [3, 7, 14, 28]
-                            
-                            })
-    
-    ##############################################################
-    
-    print("# Tuning hyper-parameters ")
-    print()
-    
-    # OMP
-    grdsurch = GridSearchCV(OrthogonalMatchingPursuit(n_nonzero_coefs=None, 
-                                                      tol=None, fit_intercept=True, 
-                                                      normalize=True, precompute='auto'), 
-                       tuned_parameters, 
-                       cv=3, 
-                       n_jobs=-1, 
-                       scoring=rmse_scorer)
-    
-    print('Starting grdsurch.fit(X_train, y_train)')
-    
-    grdsurch.fit(X_train, y_train)
-
-    print("\nBest parameters set found on development set:")
-    print()
-    print(grdsurch.best_params_)
-    
-    print(grdsurch.best_estimator_)
-    print()
-    rmse_cv = grdsurch.best_score_
-
-    #Reporting Score on Test Set
-    model               = grdsurch.best_estimator_
-    reporting_testscore = rmse_scorer(model, X_test, y_test)
-    
-    
-    return {filename: {'train_rmse_cv_4_picking': rmse_cv, 
-                       'test_rmse_4_reporting': reporting_testscore, 
-                       'test_mean_y_4_comparing': y_test.mean(),
-                       'model': model
-                      }}
-
-
-# In[49]:
-
-#3
-def GradientBoostingCVModel(filename):
-    #open file and get the dictionary
-    import pickle
-    from sklearn import preprocessing
-    from sklearn.ensemble import GradientBoostingRegressor
-    from numpy.random import RandomState
-    from sklearn.model_selection import GridSearchCV
-
-    with open(filename, 'rb') as handle:
-        data = pickle.load(handle)
-
-    #extract X_train, y_train, X_test, t_test
-    X_train = data['X_train']
-    y_train = data['y_train']
-    X_test = data['X_test']
-    y_test = data['y_test']
-    print("Dataset size read: train %d and test %d \n" %(len(y_train), len(y_test)))
-    
-    #Normalize
-    X_train = preprocessing.normalize(X_train, norm='l1')
-    X_test  = preprocessing.normalize(X_test,  norm='l1')
-    
-    ##############################################################
-    tuned_parameters = [     {  "n_estimators" :[6000],
-                               "loss" : ['ls'],
-                                "learning_rate": [0.005, 0.01, 0.001]
-                             }
-                       ]
-
-    
-    ##############################################################
-    
-    print("# Tuning hyper-parameters ")
-    print()
-
-    #Boosting
-    grdsurch = GridSearchCV(GradientBoostingRegressor(loss='ls', learning_rate=0.1, n_estimators=6000, subsample=1.0, 
-                                                      criterion='friedman_mse', min_samples_split=2, min_samples_leaf=1, 
-                                                      min_weight_fraction_leaf=0.0, max_depth=None, 
-                                                      min_impurity_split=1e-07, 
-                                                      init=None, random_state=None, max_features=None, alpha=0.9, 
-                                                      verbose=0, max_leaf_nodes=None, warm_start=False, presort='auto'), 
-                       tuned_parameters, 
-                       cv=3, 
-                       n_jobs=-1, 
-                       scoring=rmse_scorer)
-    print('Starting grdsurch.fit(X_train, y_train)')
-    
-    grdsurch.fit(X_train, y_train)
-
-    print("\nBest parameters set found on development set:")
-    print()
-    print(grdsurch.best_params_)
-    
-    print(grdsurch.best_estimator_)
-    print()
-    rmse_cv = grdsurch.best_score_
-
-    #Reporting Score on Test Set
-    model               = grdsurch.best_estimator_
-    reporting_testscore = rmse_scorer(model, X_test, y_test)
-    
-    return {filename: {'train_rmse_cv_4_picking': rmse_cv, 
-                       'test_rmse_4_reporting': reporting_testscore, 
-                       'test_mean_y_4_comparing': y_test.mean(),
-                       'model': model
-                      }}
-
-
-# In[37]:
+# In[54]:
 
 #4
 def RandomForestCVModel(filename):
@@ -277,11 +66,7 @@ def RandomForestCVModel(filename):
     ##############################################################
     tuned_parameters = []
     tuned_parameters.append( { 
-                              "n_estimators" :[2000, 4000, 6000],
-                              "min_samples_split": [2],
-                              "min_samples_leaf": [1,2],
-                              "min_weight_fraction_leaf": [0.0],
-                              "max_features": ["auto"],
+                              "n_estimators" :[2000, 4000, 6000]
                             })
     
     ##############################################################
@@ -743,7 +528,223 @@ def SVRRbfCVModel(filename):
                       }}
 
 
+# In[53]:
+
+## OK below this
+
+
 # In[ ]:
 
 
+
+
+# In[50]:
+
+#1 OK
+def LassoCVModel(filename):
+    #open file and get the dictionary
+    import pickle
+    from sklearn.linear_model import Lasso
+    from sklearn.metrics import mean_squared_error
+    from sklearn import preprocessing
+    from sklearn.model_selection import GridSearchCV
+
+    with open(filename, 'rb') as handle:
+        data = pickle.load(handle)
+
+    #extract X_train, y_train, X_test, t_test
+    X_train = data['X_train']
+    y_train = data['y_train']
+    X_test = data['X_test']
+    y_test = data['y_test']
+    print("Dataset size read: train %d and test %d \n" %(len(y_train), len(y_test)))
+    
+    #Normalize
+    X_train = preprocessing.normalize(X_train, norm='l1')
+    X_test  = preprocessing.normalize(X_test,  norm='l1')
+    
+    ##############################################################
+    tuned_parameters = []
+    tuned_parameters.append( {'alpha' : np.logspace(-4, 10, 30),
+                              'precompute' : [True, False]
+                             } )
+    
+    ##############################################################
+    
+    print("# Tuning hyper-parameters ")
+    print()
+
+    # Lasso
+    grdsurch = GridSearchCV(Lasso(alpha=1.0, fit_intercept=True, normalize=False, precompute=False, 
+                                  copy_X=True, max_iter=1e7, tol=1e-6, warm_start=False, 
+                                  positive=False, random_state=None, selection='random'), 
+                       tuned_parameters, 
+                       cv=3, 
+                       n_jobs=-1, 
+                       scoring=rmse_scorer)
+    print('Starting grdsurch.fit(X_train, y_train)')
+    
+    grdsurch.fit(X_train, y_train)
+
+    print("\nBest parameters set found on development set:")
+    print()
+    print(grdsurch.best_params_)
+    
+    print(grdsurch.best_estimator_)
+    print()
+    rmse_cv = grdsurch.best_score_
+
+    #Reporting Score on Test Set
+    model               = grdsurch.best_estimator_
+    reporting_testscore = rmse_scorer(model, X_test, y_test)
+    
+    return {filename: {'train_rmse_cv_4_picking': rmse_cv, 
+                       'test_rmse_4_reporting': reporting_testscore, 
+                       'test_mean_y_4_comparing': y_test.mean(),
+                       'model': model
+                      }
+           }
+
+
+# In[52]:
+
+#2 OK
+def OMPCVModel(filename):
+    #open file and get the dictionary
+    import pickle
+    from sklearn.linear_model import OrthogonalMatchingPursuit
+    from sklearn.metrics import mean_squared_error
+    from sklearn import preprocessing
+    from sklearn.model_selection import GridSearchCV
+
+    with open(filename, 'rb') as handle:
+        data = pickle.load(handle)
+
+    #extract X_train, y_train, X_test, t_test
+    X_train = data['X_train']
+    y_train = data['y_train']
+    X_test = data['X_test']
+    y_test = data['y_test']
+    print("Dataset size read: train %d and test %d \n" %(len(y_train), len(y_test)))
+    
+    #Normalize
+    #X_train = preprocessing.normalize(X_train, norm='l1')
+    #X_test  = preprocessing.normalize(X_test,  norm='l1')
+    
+    #‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’ 
+    ##############################################################
+    tuned_parameters = []
+    tuned_parameters.append({'tol' : [1e-20, 1e-15, 1e-11],
+                             'n_nonzero_coefs' : [3, 7, 14, 28]
+                            
+                            })
+    
+    ##############################################################
+    
+    print("# Tuning hyper-parameters ")
+    print()
+    
+    # OMP
+    grdsurch = GridSearchCV(OrthogonalMatchingPursuit(n_nonzero_coefs=None, 
+                                                      tol=None, fit_intercept=True, 
+                                                      normalize=True, precompute='auto'), 
+                       tuned_parameters, 
+                       cv=3, 
+                       n_jobs=-1, 
+                       scoring=rmse_scorer)
+    
+    print('Starting grdsurch.fit(X_train, y_train)')
+    
+    grdsurch.fit(X_train, y_train)
+
+    print("\nBest parameters set found on development set:")
+    print()
+    print(grdsurch.best_params_)
+    
+    print(grdsurch.best_estimator_)
+    print()
+    rmse_cv = grdsurch.best_score_
+
+    #Reporting Score on Test Set
+    model               = grdsurch.best_estimator_
+    reporting_testscore = rmse_scorer(model, X_test, y_test)
+    
+    
+    return {filename: {'train_rmse_cv_4_picking': rmse_cv, 
+                       'test_rmse_4_reporting': reporting_testscore, 
+                       'test_mean_y_4_comparing': y_test.mean(),
+                       'model': model
+                      }}
+
+
+# In[49]:
+
+#3 OK
+def GradientBoostingCVModel(filename):
+    #open file and get the dictionary
+    import pickle
+    from sklearn import preprocessing
+    from sklearn.ensemble import GradientBoostingRegressor
+    from numpy.random import RandomState
+    from sklearn.model_selection import GridSearchCV
+
+    with open(filename, 'rb') as handle:
+        data = pickle.load(handle)
+
+    #extract X_train, y_train, X_test, t_test
+    X_train = data['X_train']
+    y_train = data['y_train']
+    X_test = data['X_test']
+    y_test = data['y_test']
+    print("Dataset size read: train %d and test %d \n" %(len(y_train), len(y_test)))
+    
+    #Normalize
+    X_train = preprocessing.normalize(X_train, norm='l1')
+    X_test  = preprocessing.normalize(X_test,  norm='l1')
+    
+    ##############################################################
+    tuned_parameters = [     {  "n_estimators" :[6000],
+                               "loss" : ['ls'],
+                                "learning_rate": [0.005, 0.01, 0.001]
+                             }
+                       ]
+
+    
+    ##############################################################
+    
+    print("# Tuning hyper-parameters ")
+    print()
+
+    #Boosting
+    grdsurch = GridSearchCV(GradientBoostingRegressor(loss='ls', learning_rate=0.1, n_estimators=6000, subsample=1.0, 
+                                                      criterion='friedman_mse', min_samples_split=2, min_samples_leaf=1, 
+                                                      min_weight_fraction_leaf=0.0, max_depth=None, 
+                                                      min_impurity_split=1e-07, 
+                                                      init=None, random_state=None, max_features=None, alpha=0.9, 
+                                                      verbose=0, max_leaf_nodes=None, warm_start=False, presort='auto'), 
+                       tuned_parameters, 
+                       cv=3, 
+                       n_jobs=-1, 
+                       scoring=rmse_scorer)
+    print('Starting grdsurch.fit(X_train, y_train)')
+    
+    grdsurch.fit(X_train, y_train)
+
+    print("\nBest parameters set found on development set:")
+    print()
+    print(grdsurch.best_params_)
+    
+    print(grdsurch.best_estimator_)
+    print()
+    rmse_cv = grdsurch.best_score_
+
+    #Reporting Score on Test Set
+    model               = grdsurch.best_estimator_
+    reporting_testscore = rmse_scorer(model, X_test, y_test)
+    
+    return {filename: {'train_rmse_cv_4_picking': rmse_cv, 
+                       'test_rmse_4_reporting': reporting_testscore, 
+                       'test_mean_y_4_comparing': y_test.mean(),
+                       'model': model
+                      }}
 
